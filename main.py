0f53600fcf029011776d0a25af3fe37c36a66d93
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-11-11 13:14:34 -0600

    implement view in main

--- main.py
+++ main.py
@@ -4,13 +4,18 @@
 # Import libraries and modules 
 import logging, time, requests, os, re
 
+# Library to scrapp
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
+# View libraries
+import tkinter as tk
+from tkinter import scrolledtext
+
 logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s -  %(levelname)s -   %(message)s')
-logging.disable(logging.DEBUG)
+logging.disable(logging.CRITICAL)
 
 def clean_file_names(folderPath):
     os.chdir(folderPath)
@@ -20,13 +25,13 @@ def clean_file_names(folderPath):
         i += 1
 
 def get_profile_name(profile):
-    profileInfo = profile.find_element(By.CLASS_NAME, 'xdj266r') # find the number of posts
+    profileInfo = profile.find_element(By.CLASS_NAME, 'xdj266r') 
     infoByLines = str(profileInfo.text).split('\n')
     tag = infoByLines[0]
     return tag
 
 def get_number_of_posts(profile):
-    profileInfo = profile.find_element(By.CLASS_NAME, 'xdj266r') # find the number of posts
+    profileInfo = profile.find_element(By.CLASS_NAME, 'xdj266r') 
     infoByLines = str(profileInfo.text).split('\n')
     posts = infoByLines[3].split(' ')
     return int(posts[0])
@@ -46,21 +51,20 @@ def download_photos(imagesLinks, imageNames, user):
 
             with open(f"photos-{user}\\{nameDebugged}.jpg", "wb") as file:
                 file.write(response.content)
-            print(f"Image downloaded successfully!")
+            # print(f"Image downloaded successfully!")
+
         else:
             print(f"Failed to download image. Status code: {response.status_code}")
 
-def scrap(browser):
+def scrap(browser, view):
     try:
         htmlElem = browser.find_element(By.TAG_NAME, 'html')
         logging.critical("encuentra elemento html")
-        time.sleep(4)
-
-
-        accountName = get_profile_name(browser)
-        posts = get_number_of_posts(browser)
         time.sleep(2)
 
+        accountName = get_profile_name(browser)
+        # posts = get_number_of_posts(browser)
+        time.sleep(1)
 
         logging.critical("intenta scrollable")
 
@@ -82,7 +86,6 @@ def scrap(browser):
                 """, scrollable_div)
             
             if at_bottom:
-                logging.info("End page.")
                 Flag = False  
             
             else:
@@ -104,32 +107,64 @@ def scrap(browser):
                 for _ in range(3):
                     htmlElem.send_keys(Keys.END)
                     time.sleep(2)
+
         clean_file_names(f'photos-' + accountName)
+
+        label_title4 = tk.Label(view, text=f"{len(os.listdir(os.curdir))} photos downloaded... \nThanks for scrapping with us, come again soon!!<3<3 ", font=("Arial", 10), fg="white", bg="black")
+        label_title4.pack(padx=10, pady=10)
 
     except NoSuchElementException:
         print("Was not able to find an element with that class name.")
 
+def view(browser):
+
+    # main view
+    view = tk.Tk()
+    view.title("INSTAGRAM PHOTO-SCRAPPER")
+    view.geometry("500x300")  
+    view.configure(bg="black")  
+    view.resizable(False, False)
+
+    # set view icon
+    icono = tk.PhotoImage(file="pank.png")  
+    view.iconphoto(False, icono)
+
+    # label for the title
+    label_title = tk.Label(view, text="INSTAGRAM PHOTO-SCRAPPER", font=("Arial", 14), fg="white", bg="black")
+    label_title.pack(padx=10, pady=5)
+
+    readme_info = f"Please checkout the README file before using the script :)."
+    label_title2 = tk.Label(view, text=readme_info, font=("Arial", 6), fg="white", bg="black")
+    label_title2.pack(padx=10, pady=10)
+
+    info = r"""
+    Login into your account and then go to the profile you want to scrapp.
+    When you are ready, press ENTER in the console to begin the downloads."""
+    label_title3 = tk.Label(view, text=info, font=("Arial", 10), fg="white", bg="black")
+    label_title3.pack(padx=10, pady=10)
+    
+
+    # button to start scrapping
+    btn_start_scrapping = tk.Button(view, text="Iniciar Scrap", command = lambda: scrap(browser, view), fg="white", bg="black", border=2)
+    btn_start_scrapping.pack(padx= 10, pady=10)
+
+    # Packing elements
+
+    
+
+    # main view loop
+    view.mainloop()
+
+
 def main():
-    print("\n--- Please checkout the README file before using the script :). ---\n")
-    print("Login into your account and then go to the profile you want to scrapp.")
-    print("When you are ready, press ENTER in the console to begin the downloads.\n\n")
 
     # Initialize browser object
     browser = webdriver.Chrome()
     browser.get('https://www.instagram.com/')
-    time.sleep(2)
-
-    input()
-    scrap(browser)
-
-    print(f"\n\n{len(os.listdir(os.curdir))} photos downloaded...")
-    print("Thanks for scrapping with us, come again soon!!<3<3 \n\n")
-
-
+    
+    view(browser)
 
     browser.quit()
-
-
 
 if __name__ == '__main__':
     main()
